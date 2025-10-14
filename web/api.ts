b431507b196fc88621900c98c85e6f4696fa5890
haroonaslam <haroon.aslam@codup.co>
2025-10-14 19:17:59 +0500

    Remove Gadget-specific type declarations and global interfaces from gadget.d.ts

--- web/api.ts
+++ web/api.ts
@@ -1,4 +1,3 @@
-/// <reference path="./gadget.d.ts" />
 // Sets up the API client for interacting with your backend.
 // For your API reference, visit: https://docs.gadget.dev/api/extend-commerce-product-table-test
 import { Client } from "@gadget-client/extend-commerce-product-table-test";
